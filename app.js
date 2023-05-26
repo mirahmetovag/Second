c7108587888f2df63dbb2de293cefe0d40ba790d
Guzal <mirahmetovaguzal@gmail.com>
2023-05-27 02:07:26 +0500

    Second file was updated

--- app.js
+++ app.js
@@ -12,32 +12,29 @@ createServer(async (req,res)=> {
         warehouse = [];
     }
     if (req.url === '/' && req.method === 'GET') {
-        let warehouse;
-        try {
-            warehouse = JSON.parse (await fs.readFile(path, 'utf-8'));
-        } catch (error) {
-            warehouse = [];
-        }
-        res.end(JSON.stringify(warehouse));
+        res.end(JSON.stringify(warehouse.filter(n => n.amount > 0)));
     } else if (req.url = '/' && req.method === 'POST') {
         req.on('data', async (data) => {
-            const {name,amount} = JSON.parse(data);
-            let warehouse;
-        try {
-            warehouse = JSON.parse (await fs.readFile(path, 'utf-8'));
-        } catch (error) {
-            warehouse = [];
+        const {name,amount} = JSON.parse(data);
+        if (!name || !amount) {
+            return res.end('Name or Amount was not entered')
         }
         const findItem = warehouse.find((item) => item.name === name);
         if (findItem) {
-            warehouse.forEach(async(item)=>{
-            if (item.name === name) {
-                item.amount += amount;
+            if (findItem.amount + amount < 0) {
+                return res.end(`There are only ${findItem.amount}`)
+            }
+                findItem.amount += amount;
                 await fs.writeFile(path,JSON.stringify(warehouse, null,2));
                 res.end('Successfully renewed')
-            } })
         } else {
-            await fs.writeFile(path, JSON.stringify(warehouse.length? [...warehouse,{name,amount}]: [{name,amount}],null,2));
+            const id = (warehouse[warehouse.length - 1]?.id || 0) + 1;
+            warehouse.push({
+                id,
+                name,
+                amount,
+            })
+            await fs.writeFile(path, JSON.stringify(warehouse,null,2));
             res.end('Successfully added')
         }
         })
